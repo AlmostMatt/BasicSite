cd2107190943ac8847a5654e646e6216e9bc7999
Matthew Hyndman <myndman@gmail.com>
2017-02-04 15:00:22 -0800

    minimal changes

--- almostmatt/settings.py
+++ almostmatt/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'bootstrap3',
+    'EternalDraft',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- almostmatt/urls.py
+++ almostmatt/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'almostmatt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^eternal/', include('EternalDraft.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
